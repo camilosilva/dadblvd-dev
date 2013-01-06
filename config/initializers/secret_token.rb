a24a51210de89c99bf65f9299364c7102378eef3
Camilo Silva <silva.creative@gmail.com>
2013-01-06 00:18:33 -0500

    first comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReinstalllerDemo::Application.config.secret_token = '5f5acfd48cce3131c56483b7cc8b6c67fb0a9f3b86f6ca93a3c450b510cc16ece6d94cf9eeae1ca0230cd494c1b434a4c2831f6ee562c4da210569815ae3d5b9'
